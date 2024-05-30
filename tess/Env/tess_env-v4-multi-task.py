8cb81496ad2a77bd35200e26d53ec723e03b94f5
Shi-Soul <993520026@qq.com>
2024-05-30 07:29:32 +0000

    tess: clean up go back to longpenalty2

--- tess/Env/tess_env-v4-multi-task.py
+++ tess/Env/tess_env-v4-multi-task.py
@@ -7,7 +7,7 @@ import cv2
 import queue
 import random
 
-NUM_CLEANUP_HIS = 100
+NUM_CLEANUP_HIS = 10
 
 class TessEnv(gym.Env):
 
@@ -166,7 +166,7 @@ class TessEnv(gym.Env):
                 # self.rewards[ind][0] += .1
                 
                 # for ind,history in enumerate(self.clean_up_histories):
-                hist_list = sum(list(self.clean_up_histories[ind].queue)) /0.01
+                hist_list = sum(list(self.clean_up_histories[ind].queue)) /0.01 /2
                 scaled_rew = (np.tanh(NUM_CLEANUP_HIS-  hist_list)+1)/2
                 self.rewards[ind][0] += scaled_rew*0.1
                     # The more you clean, the less rewards you get when you clean
@@ -177,7 +177,7 @@ class TessEnv(gym.Env):
                 # if you are hardworking, you get a little more rewards when **someone** eat
                 for ind,history in enumerate(self.clean_up_histories):
                     hist_list = sum(list(history.queue)) 
-                    self.rewards[ind][0] += hist_list*0.01
+                    self.rewards[ind][0] += hist_list 
                 self.rewards[theind][1] += .1
                 self.real_rewards[theind] += 1
 
@@ -287,4 +287,3 @@ class TessEnv(gym.Env):
                 #self.rewards[target_id] -= 1
             
             
-
