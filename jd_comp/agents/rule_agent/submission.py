c85f3185376bd492c6cf4ac44d5308be0f3bb7c0
loping151 <wangkailing151@gmail.com>
2024-05-31 14:28:18 +0800

    failure movement

--- jd_comp/agents/rule_agent/submission.py
+++ jd_comp/agents/rule_agent/submission.py
@@ -95,7 +95,7 @@ def get_policy(coor):
         target = (4, 1)
     elif coor < 5/6:
         target = (5, 1)
-    elif coor < 6/7:
+    else: # coor < 6/7
         target = (6, 1)
     return np.array(target)
 
@@ -104,18 +104,18 @@ def get_policy(coor):
 class Memory:
     def __init__(self):
         self.global_img = 0
-        self.map_memo = []
         self.local_position = None
         self.local_direction = None
-        self.explore_step = None
+        self.last_move = None # last action
         self.position_memo = [] # to remember the last matches for localization
         self.apath = None # cached path for path planning towards centers
-        self.betray = True
         self.collect_target = np.array([2, 1])
         self.collect_priority = None
         self.inventory = None
+        self.valid_inventory = None
         self.oppo_info = [] # opponent info, coor prob
         self.memory_map = GLOBAL_MAP
+        self.last_observation = None
     
     def update_collect(self):
         diff = self.collect_target - self.inventory if self.inventory is not None else np.array([1, 1])
@@ -141,7 +141,7 @@ class Memory:
     def policy_update(self, rw):
         if rw == 0:
             return # waiting for game
-        self_coor = self.inventory[0] / sum(self.inventory)
+        self_coor = self.valid_inventory[0] / sum(self.valid_inventory)
         oppo_coor_prob = oppo_coor(self_coor, rw)
         self.oppo_info.append(oppo_coor_prob)
         if DEBUG:
@@ -156,10 +156,9 @@ class Memory:
         self.collect_target = get_policy(best_coor)
         
     def reset(self):
-        self.map_memo = []
         self.local_position = None
         self.local_direction = None
-        self.explore_step = None
+        self.last_move = None
         self.position_memo = []
         self.apath = None
         self.collect_priority = None
@@ -169,7 +168,6 @@ class Memory:
 memory = Memory()
 DEBUG = False
 if DEBUG:
-    gt_time = 0
     from PIL import Image
 
 
@@ -180,15 +178,14 @@ def my_controller(observation, action_space, is_act_continuous=False):
     global memory, gt_time
     
     if DEBUG:
-        print(perf_counter()-gt_time)
-        gt_time = perf_counter()
-    
-    if DEBUG:
         print('INVENTORY', memory.inventory)
     memory.inventory = observation['INVENTORY']
+    if sum(memory.inventory) > 2:
+        memory.valid_inventory = memory.inventory
     rgb = observation['RGB']
     rgb_grid = downsample_image(rgb, 8)
     grid_info = convert_grid_to_info(rgb_grid)
+    memory.last_observation = grid_info
     
     if DEBUG:
         print('REWARD', observation['REWARD'])
@@ -198,15 +195,28 @@ def my_controller(observation, action_space, is_act_continuous=False):
         # Image.fromarray(rgb_grid).save(f'./img_logs/{memory.global_img}_grid.png')
         memory.global_img += 1
     
-    if np.sum(rgb_grid)==0 or observation['REWARD']>0: # the rgb will be all black
+    # detect new game or new scenario
+    if observation['REWARD']>0: # the rgb will be all black
         if DEBUG:
-            print('NEW GAME! Waiting.')
+            print('NEW GAME! Reset.')
         memory.reset()
         memory.policy_update(observation['REWARD'])
         return action_to_one_hot(ACTION_TO_IDX['NOOP'])
-    if not check_location_correct(grid_info): # a new scenario starts
+    
+    elif np.sum(rgb_grid)==0:
         if DEBUG:
-            print('NEW SCENARIO! Resetting.')
+            print('NEW GAME! Reset.')
+        return action_to_one_hot(ACTION_TO_IDX['NOOP'])
+        
+    # detect if move falure
+    if check_move_failure(grid_info):
+        if DEBUG:
+            print('MOVE FAILURE!')
+        return action_to_one_hot(ACTION_TO_IDX['NOOP'])
+            
+    if not check_location_correct(grid_info, memory.local_position, memory.local_direction): # a new scenario starts
+        if DEBUG:
+            print('NEW SCENARIO or Crashed! Clean Cache.')
         memory = Memory()
         
     # grid info example: [['EMPTY', 'WALL', 'EMPTY', 'BLUE', 'BLUE'], ['EMPTY', 'WALL', 'EMPTY', 'RED', 'RED'], ['EMPTY', 'EMPTY', 'EMPTY', 'RED', 'RED'], ['EMPTY', 'EMPTY', 'SELF', 'EMPTY', 'EMPTY'], ['EMPTY', 'WALL', 'EMPTY', 'EMPTY', 'WALL']]
@@ -295,6 +305,16 @@ def can_hit(grid_info):
     return False
 
 
+def grid_info_equal(grid_info1, grid_info2):
+    '''Check if two grid info are the same'''
+    row, col = len(grid_info1), len(grid_info1[0])
+    for i in range(row):
+        for j in range(col):
+            if grid_info1[i][j] != grid_info2[i][j]:
+                return False
+    return True
+
+
 def in_back(grid_info, target):
     '''Only check the back 1 line of the agent, used to turn agent around'''
     return target in grid_info[-1]
@@ -342,37 +362,60 @@ def info2mask(info):
     return mask
 
 
-def check_location_correct(grid_info):
+def check_location_correct(grid_info, position=None, direction=None):
     '''
     Check if the local map is correctly located in the global map.
     The game will end if timestamp is over, but the agent do not know when a new scenario starts.
     So if mislocated, we reset the memory.
     '''
     global memory    
-    if memory.local_position is None:
+    if position is None:
         return True
     
     grid_info_mask = info2mask(grid_info)
-    ax, ay = memory.local_position
-    if memory.local_direction == 'up':
+    ax, ay = position
+    if direction == 'up':
         global_submap = GLOBAL_MAP[ax-3:ax+2, ay-2:ay+3]
-    elif memory.local_direction == 'left':
+    elif direction == 'left':
         global_submap = GLOBAL_MAP[ax-2:ax+3, ay-3:ay+2]
         grid_info_mask = np.rot90(grid_info_mask, k=1)
-    elif memory.local_direction == 'down':
+    elif direction == 'down':
         global_submap = GLOBAL_MAP[ax-1:ax+4, ay-2:ay+3]
         grid_info_mask = np.rot90(grid_info_mask, k=2)
-    elif memory.local_direction == 'right':
+    elif direction == 'right':
         global_submap = GLOBAL_MAP[ax-2:ax+3, ay-1:ay+4]
         grid_info_mask = np.rot90(grid_info_mask, k=3)
     return np.array_equal(global_submap, grid_info_mask)
 
+
+def check_move_failure(grid_info):
+    '''
+    True if move failure, False if not. This is used to detect if the agent is shot.
+    '''
+    global memory
+    if memory.local_position is None:
+        return False
+    
+    last_action = memory.last_move
+    if last_action == "TURN_LEFT":
+        last_dir = {'left': 'up', 'down': 'left', 'right': 'down', 'up': 'right'}
+        pos, direction = memory.local_position, last_dir[memory.local_direction] 
+    elif last_action in ['FORWARD', 'BACKWARD', 'STEP_LEFT', 'STEP_RIGHT']:
+        pos, direction = memory.local_position - np.array(action2movement(last_action, memory.local_direction)), memory.local_direction
+    else:
+        return False
+    failure =  check_location_correct(grid_info, pos, direction)
+    if failure:
+        memory.local_position, memory.local_direction = pos, direction
+    return failure
+        
 
 def find_matches(global_map, local_map, last_matches=None):
     '''Hentai code for localization. Very naive.'''
     local_map = info2mask(local_map)
     adject = [(3, 2), (2, 3), (1, 2), (2, 1)]
     matches = []
+    matches_all = []
     global_map_height, global_map_width = global_map.shape
 
     for rotation in range(4):
@@ -380,6 +423,7 @@ def find_matches(global_map, local_map, last_matches=None):
         rotated_map_height, rotated_map_width = rotated_map.shape
         
         match_list = []
+        match_list_all = []
         for row in range(global_map_height - rotated_map_height + 1):
             for col in range(global_map_width - rotated_map_width + 1):
                 sub_map = global_map[row:row + rotated_map_height, col:col + rotated_map_width]
@@ -395,7 +439,13 @@ def find_matches(global_map, local_map, last_matches=None):
                                 break
                     else:
                         match_list.append(match)
+                    match_list_all.append(match)
         matches.append(match_list)
+        matches_all.append(match_list_all)
+    
+    # potential error
+    if len(matches[0]+matches[1]+matches[2]+matches[3]) == 0:
+        matches = matches_all
 
     return matches # up left down right
 
@@ -437,12 +487,12 @@ def localization_phase(grid_info):
             if not feasible_empty[(idx+2)%4]: # opposite direction is also wall
                 for _ in range(3):
                     feasible_list.append(id2action[idx]) # more willing to keep from the wall
-    if memory.explore_step is not None and memory.explore_step in feasible_list:
+    if memory.last_move is not None and memory.last_move in feasible_list:
         for _ in range(3):
-            feasible_list.append(memory.explore_step)
+            feasible_list.append(memory.last_move)
         
     action = random.choice(feasible_list)
-    memory.explore_step = action
+    memory.last_move = action
     if is_localized:
         memory.local_position += np.array(action2movement(action, memory.local_direction))
     return action_to_one_hot(ACTION_TO_IDX[action])
@@ -543,24 +593,28 @@ def path_planning_phase(grid_info, observation):
     else: # go to center and wait
         memory.memory_map = add_card_as_obstacle(grid_info, ['RED', 'BLUE']) # ensure not collect card
         if can_hit(grid_info) and observation['READY_TO_SHOOT']:
+            memory.last_move = ''
             return action_to_one_hot(ACTION_TO_IDX['INTERACT'])
         elif 'OTHER' in scan_grid_info(grid_info): # go to
             if in_back(grid_info, 'OTHER'):
                 next_direction = {'up': 'left', 'left': 'down', 'down': 'right', 'right': 'up'}
                 memory.local_direction = next_direction[memory.local_direction]
+                memory.last_move = 'TURN_LEFT'
                 return action_to_one_hot(ACTION_TO_IDX['TURN_LEFT'])
             memory.apath = a_star_search(memory.local_position, nearest_card(grid_info, 'OTHER'), grid=memory.memory_map)
         else:
             global_center = (9, 13)
             memory.apath = a_star_search(memory.local_position, global_center, memory.memory_map)
             if len(memory.apath) == 0: # already at the center
-                return action_to_one_hot(ACTION_TO_IDX['INTERACT']) if observation['READY_TO_SHOOT'] else action_to_one_hot(ACTION_TO_IDX['TURN_LEFT'])
+                memory.last_move = ''
+                return action_to_one_hot(ACTION_TO_IDX['INTERACT'])
         # The policy will fail if the opponent do not come to center
         
     next_position = memory.apath.pop(0)
     agent_action = movement2action(tuple(np.array(next_position) - np.array(memory.local_position)), memory.local_direction)
     
     memory.local_position += np.array(action2movement(agent_action, memory.local_direction))
+    memory.last_move = agent_action
     return action_to_one_hot(ACTION_TO_IDX[agent_action])
 
 
